b86b6cc0fb31657dacb6ee72bc26817af4bff298
sas <sas@fmail.com>
2025-05-18 16:13:08 +0530

    updated properties

---
+++ benefit-issuance/src/main/java/com/issuance/configuration/OpenTelemetryConfig.java
@@ -0,0 +1,33 @@
+package com.issuance.configuration;
+
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenTelemetryConfig {
+
+    @Bean
+    public OpenTelemetry openTelemetry() {
+        ZipkinSpanExporter zipkinExporter = ZipkinSpanExporter.builder()
+            .setEndpoint("http://localhost:9411/api/v2/spans")
+            .build();
+
+        SdkTracerProvider tracerProvider = SdkTracerProvider.builder()
+            .addSpanProcessor(BatchSpanProcessor.builder(zipkinExporter).build())
+            .build();
+
+        OpenTelemetrySdk openTelemetrySdk = OpenTelemetrySdk.builder()
+            .setTracerProvider(tracerProvider)
+            .build();
+
+        GlobalOpenTelemetry.set(openTelemetrySdk);
+
+        return openTelemetrySdk;
+    }
+}
